8d47d09eae634f108f25133c5a382b8a721d4430
Sam Boysel <sboysel@gmail.com>
2017-06-03 01:42:36 -0700

    simplify fredr_docs

--- R/fredr_docs.R
+++ R/fredr_docs.R
@@ -3,10 +3,10 @@
 #' Opens FRED API web documentation in a new browser tab.
 #'
 #' @param endpoint A string representing the desired documentation for the exact
-#'				FRED API endpoint.  Default is 'base' and will open a link to
+#'				FRED API endpoint.  Default is \code{"base"}, which will open a link to
 #'				\url{https://api.stlouisfed.org/docs/fred/}.
 #' @param params A boolean value.  If \code{TRUE}, the documentation will be opened at
-#'        the 'Paramters' section.  Default is \code{FALSE}.
+#'        the 'Parameters' section.  Default is \code{FALSE}.
 #' @param debug A boolean value.  If \code{TRUE}, the documentation is not opened in a
 #'        browser.  Default is \code{FALSE}.
 #'
@@ -21,45 +21,29 @@
 #' }
 #' @export
 fredr_docs <- function(endpoint = "base", params = FALSE, debug = FALSE) {
+
+  stopifnot(length(endpoint) == 1, is.character(endpoint))
+
   base <- "https://api.stlouisfed.org/docs/fred/"
-  doc <- switch(endpoint,
-            "base" = base,
-            "category" = paste0(base, "category.html"),
-            "category/children" = paste0(base, "category_children.html"),
-            "category/related" = paste0(base, "category_related.html"),
-            "category/tags" = paste0(base, "category_tags.html"),
-            "category/related_tags" = paste0(base, "category_related_tags.html"),
-            "releases" = paste0(base, "releases.html"),
-            "releases/dates" = paste0(base, "releases_dates.html"),
-            "release" = paste0(base, "release.html"),
-            "release/dates" = paste0(base, "release_date.html"),
-            "release/series" = paste0(base, "release_series.html"),
-            "release/sources" = paste0(base, "release_sources.html"),
-            "release/tags" = paste0(base, "release_tags.html"),
-            "release/related_tags" = paste0(base, "release_related_tags.html"),
-            "series" = paste0(base, "series.html"),
-            "series/categories" = paste0(base, "series_categories.html"),
-            "series/observations" = paste0(base, "series_observations.html"),
-            "series/release" = paste0(base, "series_release.html"),
-            "series/search" = paste0(base, "series_search.html"),
-            "series/search/tags" = paste0(base, "series_search_tags.html"),
-            "series/search/related_tags" = paste0(base, "series_search_releated_tags.html"),
-            "series/tags" = paste0(base, "series_tags.html"),
-            "series/updates" = paste0(base, "series_updates.html"),
-            "series/vintagedates" = paste0(base, "series_vintagedates.html"),
-            "sources" = paste0(base, "sources.html"),
-            "source" = paste0(base, "source.html"),
-            "source/releases" = paste0(base, "source_releases.html"),
-            "tags" = paste0(base, "tags.html"),
-            "related_tags" = paste0(base, "related_tags.html"),
-            "tags/series" = paste0(base, "tags_series.html"))
-  if (is.null(doc)) {
-    stop("Invalid endpoint.")
-  }
-  if (params) {
-    doc <- paste0(doc, "#Parameters")
-  }
-  if (!debug) {
+
+  if (endpoint == "base" & !debug) {
     utils::browseURL(url = doc)
+  } else {
+
+    doc <- paste0(base, gsub("/", "_", endpoint), ".html")
+
+    if (!identical(httr::status_code(httr::GET(url = doc)), 200L)) {
+      stop(paste("Bad endpoint:", doc))
+    }
+
+    if (params) {
+      doc <- paste0(doc, "#Parameters")
+    }
+
+    if (!debug) {
+      utils::browseURL(url = doc)
+    }
+
   }
+
 }
--- tests/testthat/test_docs.R
+++ tests/testthat/test_docs.R
@@ -2,6 +2,8 @@ library(fredr)
 context("fredr_docs")
 
 test_that("fredr_docs throws errors on invalid endpoints", {
+  expect_error(fredr_docs(endpoint = 1, debug = TRUE))
+  expect_error(fredr_docs(endpoint = c("a", "b"), debug = TRUE))
   expect_error(fredr_docs(endpoint = "foo", debug = TRUE))
   expect_silent(fredr_docs(endpoint = "series",  debug = TRUE))
   expect_silent(fredr_docs(endpoint = "series", params = TRUE, debug = TRUE))
