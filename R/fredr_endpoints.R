f415964529263ee6c807376d6fd4dad0a469b242
Sam Boysel <sboysel@gmail.com>
2017-12-23 18:26:46 -0800

    add endpoints documentation object

---
+++ R/fredr_endpoints.R
@@ -0,0 +1,14 @@
+#' List of available FRED API endpoints.
+#'
+#' @format A data frame with 31 rows and 3 variables:
+#' \describe{
+#'   \item{type}{endpoint type (e.g. "Categories", "Releases", "Series", "Sources", and "Tags".)}
+#'   \item{endpoint}{endpoint name (e.g. "fred/category", "fredr/series/observations", "fredr/tags").
+#'   This name can be supplied to the \code{endpoint} parameter in \code{\link[fredr]{fredr_docs}} to
+#'   open the \href{https://api.stlouisfed.org/docs/fred/}{FRED API endpoint documentation} in a web browser.}
+#'   \item{note}{endpoint details}
+#' }
+#'
+#' @source \url{https://api.stlouisfed.org/docs/fred/}.
+#' @seealso \code{\link[fredr]{fredr_docs}}
+"fredr_endpoints"
