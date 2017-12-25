d5d5e835566d496978959f1c940b1e2fa37651bc
Sam Boysel <sboysel@gmail.com>
2017-12-25 14:07:07 -0800

    unify parameters of search functions

--- R/fredr_search.R
+++ R/fredr_search.R
@@ -4,8 +4,10 @@
 #' (\code{fredr_search}), by series ID (\code{fredr_search_id}), by series tags
 #' (\code{fredr_search_tags}), or related tags (\code{fredr_search_rel_tags}).
 #'
-#' @param search_text A string.
-#' @param series_search_text A string.
+#' @param search A string containing the search text.  Searches are performed against series
+#' attributes (\code{\link{fredr_search}}, \code{\link{fredr_search_tags}}, and
+#' \code{\link{fredr_search_rel_tags}}) or series ID
+#' (\code{\link{fredr_search_id}}).  See Details for more information.
 #' @param tag_names A string.  Note that this value must be a valid FRED series tag
 #'        (e.g. \code{30-year}, \code{usa}, \code{quarterly}, etc.).
 #' @param ... A series of named parameters to be used in the query.  Must be of the form
@@ -17,16 +19,16 @@
 #'
 #' @details Search by various series characteristics:
 #' \itemize{
-#'   \item{\code{fredr_search}}{Get economic data series that match search text (by full text of series).}
-#'   \item{\code{fredr_search_id}}{Get economic data series that match search text (by series ID).}
-#'   \item{\code{fredr_search_tags}}{Get the FRED tags for a series search.}
-#'   \item{\code{fredr_search_rel_tags}}{Get the related FRED tags for one or more FRED tags matching a series search.
+#'   \item{\code{\link{fredr_search}}}{Get economic data series that match search text (by full text of series).}
+#'   \item{\code{\link{fredr_search_id}}}{Get economic data series that match search text (by series ID).}
+#'   \item{\code{\link{fredr_search_tags}}}{Get the FRED tags for a series search.}
+#'   \item{\code{\link{fredr_search_rel_tags}}}{Get the related FRED tags for one or more FRED tags matching a series search.
 #'   See below for more information about searching for related tags.}
 #' }
 #' From the FRED API documentation on \code{series/search/related_tags}: FRED tags are
 #' attributes assigned to series. For this request, related FRED tags are the tags assigned
 #' to series that match all tags in the \code{tag_names} parameter, no tags in the \code{exclude_tag_names}
-#' parameter, and the search words set by the \code{series_search_text} parameter.
+#' parameter, and the search words set by the \code{search} parameter.
 #'
 #' @references See FRED API documentation on
 #' \href{https://research.stlouisfed.org/docs/api/fred/series_search_related_tags.html}{searching related tags}.
@@ -41,43 +43,43 @@
 #' }
 #' @name fredr_search
 #' @export
-fredr_search <- function(search_text = NULL, ...) {
-  stopifnot(!is.null(search_text))
+fredr_search <- function(search = NULL, ...) {
+  stopifnot(!is.null(search))
   fredr::fredr(
     endpoint = "series/search",
-    search_text = search_text,
+    search_text = search,
     search_type = "full_text",
     ...
   )
 }
 
 #' @rdname fredr_search
-fredr_search_id <- function(search_text = NULL, ...) {
-  stopifnot(!is.null(search_text))
+fredr_search_id <- function(search = NULL, ...) {
+  stopifnot(!is.null(search))
   fredr::fredr(
     endpoint = "series/search",
-    search_text = search_text,
+    search_text = search,
     search_type = "series_id",
     ...
   )
 }
 
 #' @rdname fredr_search
-fredr_search_tags <- function(series_search_text = NULL, ...) {
-  stopifnot(!is.null(series_search_text))
+fredr_search_tags <- function(search = NULL, ...) {
+  stopifnot(!is.null(search))
   fredr::fredr(
     endpoint = "series/search/tags",
-    series_search_text = series_search_text,
+    series_search_text = search,
     ...
   )
 }
 
 #' @rdname fredr_search
-fredr_search_rel_tags <- function(series_search_text = NULL, tag_names = NULL, ...) {
-  stopifnot(!is.null(series_search_text), !is.null(tag_names))
+fredr_search_rel_tags <- function(search = NULL, tag_names = NULL, ...) {
+  stopifnot(!is.null(search), !is.null(tag_names))
   fredr::fredr(
     endpoint = "series/search/related_tags",
-    series_search_text = series_search_text,
+    series_search_text = search,
     tag_names = tag_names,
     ...
   )
