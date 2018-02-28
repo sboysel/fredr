94f2237a99604e69b509864ac36b10dc38c6d9f2
Sam Boysel <sboysel@gmail.com>
2018-02-27 18:21:29 -0800

    make fredr_search* parameters consistent with API

--- R/fredr_search.R
+++ R/fredr_search.R
@@ -4,10 +4,10 @@
 #' (\code{fredr_search}), by series ID (\code{fredr_search_id}), by series tags
 #' (\code{fredr_search_tags}), or related tags (\code{fredr_search_rel_tags}).
 #'
-#' @param search A string containing the search text.  Searches are performed against series
-#' attributes (\code{\link{fredr_search}}, \code{\link{fredr_search_tags}}, and
-#' \code{\link{fredr_search_rel_tags}}) or series ID
-#' (\code{\link{fredr_search_id}}).  See Details for more information.
+#' @param search_text A string containing the search text.  For use with \code{\link{fredr_search}} and
+#' \code{\link{fredr_search_id}}.  See Details for more information.
+#' @param series_search_text A string containing the search text.  For use with \code{\link{fredr_search_tags}}
+#' and \code{\link{fredr_search_rel_tags}}.  See Details for more information.
 #' @param tag_names A string.  Note that this value must be a valid FRED series tag
 #'        (e.g. \code{30-year}, \code{usa}, \code{quarterly}, etc.).
 #' @param ... A series of named parameters to be used in the query.  Must be of the form
@@ -28,10 +28,11 @@
 #' From the FRED API documentation on \code{series/search/related_tags}: FRED tags are
 #' attributes assigned to series. For this request, related FRED tags are the tags assigned
 #' to series that match all tags in the \code{tag_names} parameter, no tags in the \code{exclude_tag_names}
-#' parameter, and the search words set by the \code{search} parameter.
+#' parameter, and the search words set by the \code{series_search_text} parameter.
 #'
 #' @references See FRED API documentation on
 #' \href{https://research.stlouisfed.org/docs/api/fred/series_search_related_tags.html}{searching related tags}.
+#' @seealso \code{\link{fredr_tags}}
 #'
 #' @examples
 #' \dontrun{
@@ -43,43 +44,43 @@
 #' }
 #' @name fredr_search
 #' @export
-fredr_search <- function(search = NULL, ...) {
-  stopifnot(!is.null(search))
+fredr_search <- function(search_text = NULL, ...) {
+  stopifnot(!is.null(search_text))
   fredr::fredr(
     endpoint = "series/search",
-    search_text = search,
+    search_text = search_text,
     search_type = "full_text",
     ...
   )
 }
 
 #' @rdname fredr_search
-fredr_search_id <- function(search = NULL, ...) {
-  stopifnot(!is.null(search))
+fredr_search_id <- function(search_text = NULL, ...) {
+  stopifnot(!is.null(search_text))
   fredr::fredr(
     endpoint = "series/search",
-    search_text = search,
+    search_text = search_text,
     search_type = "series_id",
     ...
   )
 }
 
 #' @rdname fredr_search
-fredr_search_tags <- function(search = NULL, ...) {
-  stopifnot(!is.null(search))
+fredr_search_tags <- function(series_search_text = NULL, ...) {
+  stopifnot(!is.null(series_search_text))
   fredr::fredr(
     endpoint = "series/search/tags",
-    series_search_text = search,
+    series_search_text = series_search_text,
     ...
   )
 }
 
 #' @rdname fredr_search
-fredr_search_rel_tags <- function(search = NULL, tag_names = NULL, ...) {
-  stopifnot(!is.null(search), !is.null(tag_names))
+fredr_search_rel_tags <- function(series_search_text = NULL, tag_names = NULL, ...) {
+  stopifnot(!is.null(series_search_text), !is.null(tag_names))
   fredr::fredr(
     endpoint = "series/search/related_tags",
-    series_search_text = search,
+    series_search_text = series_search_text,
     tag_names = tag_names,
     ...
   )
