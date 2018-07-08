a8ccb1d14b228b4aa7c0637dd4a5d57819b8c37a
Sam Boysel <sboysel@gmail.com>
2018-07-08 14:16:56 -0700

    fredr_set_key simply set environment variable

--- tests/testthat/test_key.R
+++ tests/testthat/test_key.R
@@ -1,32 +1,10 @@
 library(fredr)
 context("API Key")
 
-test_that("fredr_key throws an error if .Renviron is present.", {
-  # Backup
-  env_key <- Sys.getenv("FRED_API_KEY")
-  renv <- file.path(getwd(), ".Renviron")
-  if (file.exists(renv)) {
-    file.rename(renv, paste0(renv, ".bak"))
-  }
-  # Tests
-  Sys.setenv(FRED_API_KEY = "")
-  expect_message(fredr_key(env_key),
-                 "FRED API key successfully set.")
-  # expect_error(fredr_key(env_key))
-  expect_true(file.remove(renv))
-  Sys.setenv(FRED_API_KEY = env_key)
-  expect_message(key <- fredr_key(env_key),
-                 "FRED API key set as environment variable.")
-  expect_identical(key, env_key)
-  expect_false(file.exists(renv))
-  Sys.setenv(FRED_API_KEY = "")
-  file.create(renv)
-  expect_error(fredr_key(env_key),
-               ".Renviron file exists in directory")
-  file.remove(renv)
-  # Cleanup
-  Sys.setenv(FRED_API_KEY = env_key)
-  if (file.exists(paste0(renv, ".bak"))) {
-    file.rename(paste0(renv, ".bak"), renv)
-  }
+test_that("fredr_set_key properly sets key", {
+  backup <- Sys.getenv("FRED_API_KEY")
+  fredr_set_key("foo")
+  expect_identical(Sys.getenv("FRED_API_KEY"), "foo")
+  fredr_set_key(backup)
+  expect_identical(Sys.getenv("FRED_API_KEY"), backup)
 })
