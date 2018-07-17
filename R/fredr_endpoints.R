633992794d663c25c42c36bc69e024d7f3f8807a
Sam Boysel <sboysel@gmail.com>
2018-07-17 12:42:52 -0700

    doc improvements: fredr_endpoints

--- R/fredr_endpoints.R
+++ R/fredr_endpoints.R
@@ -4,13 +4,15 @@
 #' \describe{
 #'   \item{type}{endpoint type (e.g. "Categories", "Releases", "Series", "Sources", and "Tags".)}
 #'   \item{endpoint}{endpoint name (e.g. "fred/category", "fredr/series/observations", "fredr/tags").
-#'   This name can be supplied to the \code{endpoint} parameter in \code{\link[fredr]{fredr_docs}} to
-#'   open the \href{https://api.stlouisfed.org/docs/fred/}{FRED API endpoint documentation} in a web browser.}
+#'   This name can be supplied to the [endpoint] parameter in [fredr_docs()] to
+#'   open the [FRED API endpoint documentation](https://api.stlouisfed.org/docs/fred/)
+#'   in a web browser.}
 #'   \item{note}{endpoint details}
 #' }
 #'
 #' @section API Documentation:
 #'
 #' [FRED API](https://api.stlouisfed.org/docs/fred/)
+#'
 #' @seealso [fredr()], [fredr_docs()]
 "fredr_endpoints"
