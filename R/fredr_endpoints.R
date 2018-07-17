b7500ad3b51d666ed608ee024a73d3cc7f61713b
Sam Boysel <sboysel@gmail.com>
2018-07-17 14:09:27 -0700

    fix bad link in fredr_endpoints

--- R/fredr_endpoints.R
+++ R/fredr_endpoints.R
@@ -2,11 +2,11 @@
 #'
 #' @format A `tibble` with 31 rows and 3 variables:
 #' \describe{
-#'   \item{type}{endpoint type (e.g. "Categories", "Releases", "Series", "Sources", and "Tags".)}
 #'   \item{endpoint}{endpoint name (e.g. "fred/category", "fredr/series/observations", "fredr/tags").
-#'   This name can be supplied to the [endpoint] parameter in [fredr_docs()] to
+#'   This name can be supplied to the `endpoint` parameter in [fredr_docs()] to
 #'   open the [FRED API endpoint documentation](https://api.stlouisfed.org/docs/fred/)
 #'   in a web browser.}
+#'   \item{type}{endpoint type (e.g. "Categories", "Releases", "Series", "Sources", and "Tags".)}
 #'   \item{note}{endpoint details}
 #' }
 #'
