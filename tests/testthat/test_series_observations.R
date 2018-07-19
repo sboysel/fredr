40d977874df285017677a07ed3b5be9f48fcc73c
DavisVaughan <mvaugh15@uncc.edu>
2018-07-18 20:21:03 -0400

    Update fredr() tests to reflect tidy return format. Add test for zero row tibble.

--- tests/testthat/test_series_observations.R
+++ tests/testthat/test_series_observations.R
@@ -6,9 +6,23 @@ test_that("fredr_series_observations() returns proper object", {
   expect_silent(series <- fredr_series_observations(series_id = "GNPCA", limit = 20L))
   expect_s3_class(series, c("tbl_df", "tbl", "data.frame"))
   expect_s3_class(series$date, "Date")
-  expect_type(series[[2]], "double")
-  expect_true(ncol(series) == 2)
+  expect_type(series[[2]], "character")
+  expect_type(series[[3]], "double")
+  expect_true(ncol(series) == 3)
   expect_true(nrow(series) == 20)
+})
+
+test_that("fredr_series_observations() properly returns zero row tibble", {
+  skip_if_no_key()
+  expect_silent(
+    series <- fredr::fredr_series_observations(
+      series_id = "GNPCA",
+      observation_start = as.Date("2050-01-01")
+    )
+  )
+  expect_s3_class(series, c("tbl_df", "tbl", "data.frame"))
+  expect_true(ncol(series) == 3)
+  expect_true(nrow(series) == 0)
 })
 
 test_that("fredr_series_observations() throws errors for bad parameters", {
