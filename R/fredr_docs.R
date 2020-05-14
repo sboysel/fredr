93c4ea508e12c11b166817d9f798c21c54f20bd3
Sam Boysel <sboysel@gmail.com>
2020-05-14 15:42:04 -0700

    simplify fredr_docs() to just open base API documentation page

--- R/fredr_docs.R
+++ R/fredr_docs.R
@@ -1,16 +1,6 @@
 #' Open the web documentation for a certain FRED API topic.
 #'
-#' Opens FRED API web documentation in a new browser tab.
-#'
-#' @param endpoint A string representing the desired documentation for the exact
-#' FRED API endpoint.  Default is `"base"`, which will open a link to
-#' \url{https://api.stlouisfed.org/docs/fred/}.
-#' @param params A boolean value.  If `TRUE`, the documentation will be opened at
-#' the "Parameters" section.  Default is `FALSE`.
-#' @param debug A boolean value.  If `TRUE`, the documentation is not opened in a
-#' browser and the documentation URL is returned as a string.  If `FALSE`,
-#' documentation is opened in a browser and nothing is returned.  Default is
-#' `FALSE`.
+#' Opens FRED API web documentation in a new web browser tab.
 #'
 #' @section API Documentation:
 #'
@@ -18,40 +8,7 @@
 #'
 #' @examples
 #' fredr_docs()
-#' fredr_docs(endpoint = "category")
-#' fredr_docs(endpoint = "series/observations")
-#' fredr_docs(endpoint = "series/observations", params = TRUE)
 #' @export
-fredr_docs <- function(endpoint = "base", params = FALSE, debug = FALSE) {
-
-  stopifnot(length(endpoint) == 1, is.character(endpoint))
-
-  if (identical(endpoint, "base") & params) {
-    stop("params cannot be set to TRUE with the 'base' endpoint")
-  }
-
-  url <- "https://api.stlouisfed.org/docs/fred/"
-
-  if (!identical(endpoint, "base")) {
-    url <- paste0(url, gsub("/", "_", endpoint), ".html")
-  }
-
-  if (params) {
-    url <- paste0(url, "#Parameters")
-  }
-
-  if (debug) {
-    url
-  } else {
-
-    resp <- httr::status_code(httr::GET(url = url))
-
-    if (!identical(resp, 200L)) {
-      stop(paste("Bad endpoint:", url))
-    } else {
-      utils::browseURL(url = url)
-    }
-
-  }
-
+fredr_docs <- function() {
+  utils::browseURL(url = "https://api.stlouisfed.org/docs/fred/")
 }
--- tests/testthat/test_docs.R
+++ tests/testthat/test_docs.R
@@ -1,22 +1,6 @@
 library(fredr)
-context("Documentation functions")
+context("Documentation function")
 
-test_that("fredr_docs throws errors on invalid endpoints", {
-  expect_error(fredr_docs(endpoint = 1, debug = TRUE))
-  expect_error(fredr_docs(endpoint = c("a", "b"), debug = TRUE))
-  expect_error(fredr_docs(params = TRUE, debug = TRUE))
-  expect_error(fredr_docs(endpoint = "foo"))
-  expect_error(fredr_docs(endpoint = "foo", params = TRUE))
+test_that("fredr_docs just works...", {
   expect_silent(fredr_docs())
-  expect_silent(fredr_docs(debug = TRUE))
-  expect_silent(fredr_docs(endpoint = "foo", debug = TRUE))
-  expect_silent(fredr_docs(endpoint = "series",  debug = TRUE))
-  expect_silent(fredr_docs(endpoint = "series", params = TRUE, debug = TRUE))
-  expect_silent(fredr_docs(endpoint = "series", params = TRUE))
-})
-
-test_that("fredr_docs returns the appropriate URL when debug = TRUE", {
-  expect_identical(fredr_docs(debug = TRUE), "https://api.stlouisfed.org/docs/fred/")
-  expect_identical(fredr_docs(endpoint = "series", debug = TRUE), "https://api.stlouisfed.org/docs/fred/series.html")
-  expect_identical(fredr_docs(endpoint = "series", params = TRUE, debug = TRUE), "https://api.stlouisfed.org/docs/fred/series.html#Parameters")
 })
