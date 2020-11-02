bfade79f24ddea404f880b077a377fd68df3ff61
DavisVaughan <davis@rstudio.com>
2020-11-02 12:41:49 -0500

    Add a package doc file

---
+++ R/fredr-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+#' @aliases fredr-package
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
