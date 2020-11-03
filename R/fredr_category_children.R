b09c4f0ea11f71983e453d5838b6e052b62eb492
DavisVaughan <davis@rstudio.com>
2020-11-03 09:49:13 -0500

    Reflow documentation

--- R/fredr_category_children.R
+++ R/fredr_category_children.R
@@ -1,25 +1,26 @@
 #' Get the child categories for a specified FRED parent category
 #'
 #' @param category_id An integer ID for the category.  Default is `0` for the
-#' root category. _Required parameter._
+#'   root category. _Required parameter._
 #'
 #' @param realtime_start A `Date` indicating the start of the real-time period.
-#' Defaults to today's date. For more information, see
-#' [Real-Time Periods](https://research.stlouisfed.org/docs/api/fred/realtime_period.html).
+#'   Defaults to today's date. For more information, see [Real-Time
+#'   Periods](https://research.stlouisfed.org/docs/api/fred/realtime_period.html).
 #'
 #' @param realtime_end A `Date` indicating the end of the real-time period.
-#' Defaults to today's date. For more information, see
-#' [Real-Time Periods](https://research.stlouisfed.org/docs/api/fred/realtime_period.html).
+#'   Defaults to today's date. For more information, see [Real-Time
+#'   Periods](https://research.stlouisfed.org/docs/api/fred/realtime_period.html).
 #'
-#' @return A `tibble` object containing the name and ID for the children categories
-#' of the parent category indicated by `category_id`.
+#' @return A `tibble` object containing the name and ID for the children
+#'   categories of the parent category indicated by `category_id`.
 #'
 #' @section API Documentation:
 #'
-#' [fred/category/children](https://research.stlouisfed.org/docs/api/fred/category_children.html)
+#'   [fred/category/children](https://research.stlouisfed.org/docs/api/fred/category_children.html)
 #'
 #' @seealso [fredr_category()], [fredr_category_related()],
-#' [fredr_category_series()], [fredr_category_tags()], [fredr_category_related_tags()]
+#'   [fredr_category_series()], [fredr_category_tags()],
+#'   [fredr_category_related_tags()]
 #'
 #' @examples
 #' if (fredr_has_key()) {
